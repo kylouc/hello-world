237e68dae18a183fc3199ae0acebe30def5ab14e
kylian <kylcou.couturier@gmail.com>
2025-03-19 16:12:48 +0100

    'ok'

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+# This file produce some printing
+
+print("Hello world")
+
+print( "holla, me llamo pablo neruda")
+# We introduce some interaction with the user
+
+name = input("What's your name?")
+print("\nHello " + name)
+
